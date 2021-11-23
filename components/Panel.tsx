fdcee88174e8e2fb5782875a91c1b7cf01b437b2
Rob Murtagh <robmurtagh@googlemail.com>
2021-11-23 20:57:14 +0000

    Improve styling of panel

--- components/Panel.tsx
+++ components/Panel.tsx
@@ -3,7 +3,7 @@
  */
 export default function Panel({ children }) {
   return (
-    <div className="bg-blue-100 border-blue-500 rounded-lg text-blue-900 px-7 py-3 my-10 shadow-lg text-lg font-medium">
+    <div className="bg-blue-100 border-blue-500 border-l-4 rounded-lg px-7 py-3 my-10 shadow-lg text-lg">
       {children}
     </div>
   );
